6dacb7213794436b7602c0a1242989742e53221d
zwoong <zwoong6867@gmail.com>
2025-04-02 00:04:58 +0900

    ✨ MusicPlayerMain3

---
+++ src/oop1/MusicPlayerMain3.java
@@ -0,0 +1,49 @@
+package oop1;
+
+public class MusicPlayerMain3 {
+    public static void main(String[] args) {
+        MusicPlayerData data = new MusicPlayerData();
+
+        // 음악 플레이어 켜기
+        on(data);
+        // 볼륨 증가
+        volumeUp(data);
+        // 볼륨 증가
+        volumeUp(data);
+        // 볼룸 감소
+        volumeDown(data);
+        // 음악 플레이어 상태
+        showStatus(data);
+        // 음악 플레이어 끄기
+        off(data);
+    }
+
+    public static void on(MusicPlayerData data) {
+        data.isOn = true;
+        System.out.println("음악 플레이어를 시작합니다");
+    }
+
+    public static void off(MusicPlayerData data) {
+        data.isOn = false;
+        System.out.println("음악 플레이어를 종료합니다");
+    }
+
+    public static void volumeUp(MusicPlayerData data) {
+        data.volume++;
+        System.out.println("음악 플레이어 볼륨:" + data.volume);
+    }
+
+    public static void volumeDown(MusicPlayerData data) {
+        data.volume--;
+        System.out.println("음악 플레이어 볼륨:" + data.volume);
+    }
+
+    public static void showStatus(MusicPlayerData data) {
+        System.out.println("음악 플레이어 상태 확인");
+        if (data.isOn) {
+            System.out.println("음악 플레이어 ON, 볼륨:" + data.volume);
+        } else {
+            System.out.println("음악 플레이어 OFF");
+        }
+    }
+}
