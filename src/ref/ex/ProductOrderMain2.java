fea8fe29a43f4b81ba8f101e3990402282f11921
zwoong <zwoong6867@gmail.com>
2025-04-01 21:41:09 +0900

    ✨ ProductOrderMain2

---
+++ src/ref/ex/ProductOrderMain2.java
@@ -0,0 +1,36 @@
+package ref.ex;
+
+public class ProductOrderMain2 {
+    public static void main(String[] args) {
+        ProductOrder[] orders = new ProductOrder[3];
+        orders[0] = createOrder("두부", 2000, 2);
+        orders[1] = createOrder("김치", 5000, 1);
+        orders[2] = createOrder("콜라", 1500, 2);
+
+        printOrders(orders);
+        int totalAmount = getTotalAmount(orders);
+        System.out.println("총 결제 금액: " + totalAmount);
+    }
+
+    public static ProductOrder createOrder(String productName, int price, int quantity) {
+        ProductOrder productOrder = new ProductOrder();
+        productOrder.productName = productName;
+        productOrder.price = price;
+        productOrder.quantity = quantity;
+        return productOrder;
+    }
+
+    public static void printOrders(ProductOrder[] orders) {
+        for (ProductOrder order : orders) {
+            System.out.println("상품명: " + order.productName + ", 가격: " + order.price + ", 수량: " + order.quantity);
+        }
+    }
+
+    public static int getTotalAmount(ProductOrder[] orders) {
+        int totalAmount = 0;
+        for (ProductOrder order : orders) {
+            totalAmount += order.price * order.quantity;
+        }
+        return totalAmount;
+    }
+}
