a69a66a2e0abaea02d82d76ce1ac72d72be9e986
zwoong <zwoong6867@gmail.com>
2025-04-03 22:35:00 +0900

    ✨ MehodInitMain3

--- src/construct/MemberInit.java
+++ src/construct/MemberInit.java
@@ -4,4 +4,11 @@ public class MemberInit {
     String name;
     int age;
     int grade;
+
+    // 추가
+    void initMember(String name, int age, int grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
 }
