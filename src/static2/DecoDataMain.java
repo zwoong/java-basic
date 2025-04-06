f8eee48128136f2def10fcd0e898350e56933a4f
zwoong <zwoong6867@gmail.com>
2025-04-06 15:57:59 +0900

    ✨ CarMain

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,9 +1,13 @@
 package static2;
 
+//import static static2.DecoData.staticCall;
+
+import static static2.DecoData.*; // static import
+
 public class DecoDataMain {
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
-        DecoData.staticCall();
+        staticCall();
 
         System.out.println("2. 인스턴스 호출1");
         DecoData data1 = new DecoData();
