4018fe4a4c35bbbf1712ccdc433664361395455d
zwoong <zwoong6867@gmail.com>
2025-04-06 15:26:03 +0900

    ✨ DecoDataMain

---
+++ src/static2/DecoData.java
@@ -0,0 +1,31 @@
+package static2;
+
+public class DecoData {
+
+    private int instanceValue;
+    private static int staticValue;
+
+    public static void staticCall() {
+        // instanceValue++; // 인스턴스 변수 접근, 컴파일 에러
+        // instanceMethod(); // 인스턴스 메서드 접근, 컴파일 에러
+
+        staticValue++; // 정적 변수 접근
+        staticMethod(); // 정적 메서드 접근
+    }
+
+    public void instanceCall() {
+        instanceValue++; // 인스턴스 변수 접근
+        instanceMethod(); // 인스턴스 메서드 접근
+
+        staticValue++; // 정적 변수 접근
+        staticMethod(); // 정적 메서드 접근
+    }
+
+    private void instanceMethod() {
+        System.out.println("instanceValue=" + instanceValue);
+    }
+
+    private static void staticMethod() {
+        System.out.println("staticValue=" + staticValue);
+    }
+}
