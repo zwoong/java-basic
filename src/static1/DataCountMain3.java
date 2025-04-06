474acd2793b4a5e5e760e8e7bd217c958d359591
zwoong <zwoong6867@gmail.com>
2025-04-06 15:03:15 +0900

    ✨ DataCountMain3

--- src/static1/DataCountMain3.java
+++ src/static1/DataCountMain3.java
@@ -10,5 +10,13 @@ public class DataCountMain3 {
 
         Data3 data3 = new Data3("C");
         System.out.println("C count=" + Data3.count);
+
+        // 추가
+        // 인스턴스를 통한 접근 (권장되지않음)
+        Data3 data4 = new Data3("D");
+        System.out.println(data4.count);
+
+        // 클래시를 통한 접근
+        System.out.println(Data3.count);
     }
 }
