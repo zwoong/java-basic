6d60e600728a977590611a293ff6dd7abab3bcc5
zwoong <zwoong6867@gmail.com>
2025-04-06 10:03:47 +0900

    ✨ SpeakerMain

--- src/access/SpeakerMain.java
+++ src/access/SpeakerMain.java
@@ -13,7 +13,7 @@ public class SpeakerMain {
 
         // 필드에 직접 접근
         System.out.println("volume 필드 직접 접근 수행");
-        speaker.volume = 200;
+        // speaker.volume = 200;
         speaker.showVolume();
     }
 }
