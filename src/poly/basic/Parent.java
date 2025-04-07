4447796d80d4d1450ddadc687505e2e3ab9531ee
zwoong <zwoong6867@gmail.com>
2025-04-07 14:20:46 +0900

    ✨ CastingMain6

--- src/poly/basic/Parent.java
+++ src/poly/basic/Parent.java
@@ -2,6 +2,6 @@ package poly.basic;
 
 public class Parent {
     public void parentMethod() {
-        System.out.println("Parent,parentMethod");
+        System.out.println("Parent.parentMethod");
     }
 }
